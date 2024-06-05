5ef2cfa1a9db7e1b724f2e15d1a188e919eb5294
Chiu Chih Yung <hannibal.chiu@gmail.com>
2024-06-05 13:45:01 +0800

    Update csp.js

--- modules/csp.js
+++ modules/csp.js
@@ -1,9 +1,11 @@
 import { randomBytes } from 'crypto'
 
+const nonce = randomBytes(24).toString('base64')
+
 export default function cspModule() {
   // Set nuxt CSP options.
   this.options.render.csp = {
-    reportOnly: true,
+    reportOnly: false,
     hashAlgorithm: 'sha256',
     // Make sure unsafe-inline is always sent along with generated hashes to support browsers not compatible with CSP2.
     unsafeInlineCompatibility: true,
@@ -14,14 +16,14 @@ export default function cspModule() {
         // by nuxt.
         // Allow the execution of scripts dynamically added to the page, as long as they were loaded by a safe, already-trusted script.
         // This requires CSP3. Browsers that support this will ignore unsafe-inline, unsafe-eval, self and host based source lists.
-        "'strict-dynamic'",
+       // "'strict-dynamic'",
         // Allow unsafe inline scripts. This is used as a fallback for browsers that do not support CSP2.
         // Browsers supporting CSP2 will ignore unsafe-inline because we set a nonce and hashes.
         "'unsafe-inline'",
         // Allow scripts to be loaded from all hosts. This is used as a fallback for browsers that do not support CSP3
         // to make our site compatible with those older browsers. We don't have full protection in those older browsers.
-        'https:',
-        'http:',
+        "'self'",
+        `'nonce-${nonce}'`
       ],
       // Prevents fetching and executing plugin resources embedded using <object>, <embed> or <applet> tags.
       // The most common example is Flash.
@@ -29,24 +31,44 @@ export default function cspModule() {
       // Disables <base> URIs, preventing attackers from changing the locations of scripts loaded from relative URLs.
       'base-uri': ["'none'"],
       'require-trusted-types-for': ["'script'"],
+      'style-src': [`'nonce-${nonce}'`,"'self'", "'unsafe-inline'"],
+      'img-src': ["'self'", 'data:'],
+      'connect-src': ["'self'"],
+      'frame-src': ["'self'"],
+      'frame-ancestors': ["'self'"],
+      'font-src': ["'self'",'data:'],
+      'media-src': ["'self'"],
+      'manifest-src': ["'self'"],
+      'worker-src': ["'self'"],
+      'form-action': ["'self'"]
     },
+
+    // script-src, style-src, img-src, connect-src, frame-src, frame-ancestors, font-src, media-src, manifest-src, worker-src, form-action
   }
 
   this.nuxt.hook('vue-renderer:ssr:context', (context) => {
     // Generate a 128 bit random nonce every request.
-    const nonce = randomBytes(128).toString('base64')
+    // const nonce = randomBytes(5).toString('base64')
     // Inject nonce into vuex state before state is serialized into window.__NUXT__.
     context.nuxt.state.nonce = nonce
   })
 
   this.nuxt.hook(
     'render:route',
-    (url, { cspScriptSrcHashes }, context) => {
+    (url, result, context) => {
       // Extract nonce generated in render:routeContext.
       const nonce = context.nuxt.state.nonce
       // Add nonce to cspScriptSrcHashes. Nuxt will populate all entries in this array
       // to the csp header and meta tags as part of the script-src csp policy.
-      cspScriptSrcHashes.push(`'nonce-${nonce}'`)
+
+      result.cspScriptSrcHashes.push(`'nonce-${nonce}'`)
+      result.html  = result.html.replace(/<script/g, `<script nonce="${nonce}"`).replace(/<style/g, `<style nonce="${nonce}"`)
     }
   )
+
+  this.nuxt.hook('render:routeContext', (context) => {
+    //console.log(context)  
+  })
+
+
 }
